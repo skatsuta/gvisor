6176c8e537fe4cdc144a4d75183de492c524ef4e
Kevin Krakauer <krakauer@google.com>
2020-01-29 13:03:47 -0800

    iptables: enable the UDP matcher
    Enables the `-m udp [--dport] [--sport]` syntax in iptables.
    
    This enables gVisor to pass all the existing UDP tests in tests/iptables.
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/gvisor/pull/1683 from kevinGC:ipt-udp-matchers d6a2e01d3e57e0837c7e5cfda3b56c4dcfbb4627
    PiperOrigin-RevId: 292201825

--- pkg/tcpip/iptables/types.go
+++ pkg/tcpip/iptables/types.go
@@ -174,6 +174,8 @@ type Matcher interface {
 	// Match returns whether the packet matches and whether the packet
 	// should be "hotdropped", i.e. dropped immediately. This is usually
 	// used for suspicious packets.
+	//
+	// Precondition: packet.NetworkHeader is set.
 	Match(hook Hook, packet tcpip.PacketBuffer, interfaceName string) (matches bool, hotdrop bool)
 }
 
